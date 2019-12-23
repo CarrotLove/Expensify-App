f3e20d7de153a436fbc4ef72ef735780a89a5b54
Weiting <w6gao@ucsd.edu>
2019-12-23 10:29:24 -0800

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -15,14 +15,9 @@ const store = configureStore();
 //     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 //     console.log(visibleExpenses);
 //   });
-store.dispatch(addExpense({description:'Water bill', amount: 4500}));
-store.dispatch(addExpense({description:'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description:'Rent', amount: 3000, createdAt: 2000}));
 
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+
 
 const jsx = (
     <Provider store={store}>
