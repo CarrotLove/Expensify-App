6301b9fe9b93ee5b0b984b1f9f27961a834f95d3
Weiting <w6gao@ucsd.edu>
2019-12-24 11:52:26 -0800

    add webpack config

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,7 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">ExpenseEdit</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        
     </header>
 );
 
